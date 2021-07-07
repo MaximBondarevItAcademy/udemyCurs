a1dfa8489f96d08f4845a2bb59946835b6f87fcd
maxim <maksinn.bondarev.itacademy@bk.ru>
2021-07-07 23:45:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict"; // мы работаем в современно режиме
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`)
+
+// const user = "Ivan";
+
+// alert(`Hello,${user}`);
+
+console.log("arr" + " - Object");
+console.log(4 + " - Object");
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+console.log(incr);
