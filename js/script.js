e558d659836211b6bd158584b1fb59b6f251e0d9
maxim <maksinn.bondarev.itacademy@bk.ru>
2021-07-08 22:50:12 +0300

    Started to doing my first project

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-"use strict"; // мы работаем в современно режиме
+"use strict";
 
-// const category = 'toys';
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// console.log(`http://someurl.com/${category}/5`)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const user = "Ivan";
+const a = prompt("Один из последних просмотренных фильмов?", "");
+      b = prompt("На сколько оцените его?", "");
+      c = prompt("Один из последних просмотренных фильмов?", "");
+      d = prompt("На сколько оцените его?", "");
 
-// alert(`Hello,${user}`);
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
 
-console.log("arr" + " - Object");
-console.log(4 + " - Object");
-
-let incr = 10;
-let decr = 10;
-
-incr++;
-console.log(incr);
+      console.log(personalMovieDB);
